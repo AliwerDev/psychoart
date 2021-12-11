6b53ef5ef13947222af175557495efeefd837f3f
AliwerDev <alwerdev@gmail.com>
2021-12-11 15:26:36 +0500

    hello

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+var header = document.querySelector("header");
+
 window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+const sticky = function(){
+    header.classList.add("sticky");
+}
